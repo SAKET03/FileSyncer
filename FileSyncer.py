3fe633775b08ba80188397d7cfd172961de1fb88
Saket Sultania <saketsultania.lat@gmail.com>
2020-07-05 16:47:08 +0530

    first commit

---
+++ FileSyncer.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+from filecmp import dircmp
+
+source: str = input("Enter Source Path : ")
+destination: str = input("Enter Destination Path : ")
+extension: list = input("Enter File Format : ")
+
+result = dircmp(source, destination)
+var1 = result.left_list
+var2 = result.right_list
+diff = []
+
+for filename in var2:
+    if filename not in var1:
+        diff.append(r"{}\{}".format(destination, filename))
+print(diff)
+
+if len(diff) != 0:
+    for path in diff:
+        os.remove(path)
+else:
+    for folders, subfolders, filenames in os.walk(source):
+        for filename in filenames:
+            if filename.endswith("{}".format(extension)):
+                shutil.copy(os.path.join(folders, filename), destination)
