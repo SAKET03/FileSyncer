7ef6e6b8bbc422e8d22477371e25857c19e4f5ec
Saket Sultania <saketsultania.lat@gmail.com>
2020-07-05 20:07:55 +0530

    Bug Fixed

--- FileSyncer.py
+++ FileSyncer.py
@@ -4,7 +4,7 @@ from filecmp import dircmp
 
 source: str = input("Enter Source Path : ")
 destination: str = input("Enter Destination Path : ")
-extension: list = input("Enter File Format : ")
+extension: str = input("Enter File Format : ")
 
 result = dircmp(source, destination)
 var1 = result.left_list
@@ -23,4 +23,4 @@ if len(diff) != 0:
 for folders, subfolders, filenames in os.walk(source):
     for filename in filenames:
         if filename.endswith("{}".format(extension)):
-            shutil.copy(os.path.join(folders, filename), destination)
+            shutil.copytree(os.path.join(folders, filename), destination)
